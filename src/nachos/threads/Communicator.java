8b23df1261da525de87f85939554b08b90eaca7e
fadderman <g.falnes@stud.uis.no>
2012-09-21 15:56:20 +0200

    Perhaps finished communicator class

--- src/nachos/threads/Communicator.java
+++ src/nachos/threads/Communicator.java
@@ -10,32 +10,66 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
-    public Communicator() {
-    }
+	private Lock lock;
 
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
-    public void speak(int word) {
-    }
+	private Condition listeners;
+	private Condition speakers;
+	private Condition isDone;
+	
+	boolean full;
+	
+	int word;
 
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	return 0;
-    }
+	/**
+	 * Allocate a new communicator.
+	 */
+	public Communicator() {
+		lock = new Lock();
+        listeners = new Condition(lock);
+        speakers = new Condition(lock);
+        isDone = new Condition(lock);
+        full = false;
+	}
+
+	/**
+	 * Wait for a thread to listen through this communicator, and then transfer
+	 * <i>word</i> to the listener.
+	 *
+	 * <p>
+	 * Does not return until this thread is paired up with a listening thread.
+	 * Exactly one listener should receive <i>word</i>.
+	 *
+	 * @param	word	the integer to transfer.
+	 */
+	public void speak(int word) {
+		lock.acquire();
+		
+		while(full){
+			speakers.sleep();
+		}
+		this.word = word;
+		
+		listeners.wake();
+		lock.release();
+	}
+
+	/**
+	 * Wait for a thread to speak through this communicator, and then return
+	 * the <i>word</i> that thread passed to <tt>speak()</tt>.
+	 *
+	 * @return	the integer transferred.
+	 */    
+	public int listen() {
+		lock.acquire();
+		
+		while(full){
+			listeners.sleep();
+		}
+		int result = word;
+		full = false;
+		speakers.wake();
+		
+		lock.release();
+		return result;
+	}
 }
